276d4ac87198cf11b1403f84bc1c4cfab00b6439
Rado <rkavciak@gmail.com>
2015-09-06 19:57:29 +0200

    SQLlite insert data fixed

--- www/js/cameraAccess.js
+++ www/js/cameraAccess.js
@@ -12,9 +12,9 @@ function onDeviceReady() {
         alert("SQL database is not supported in this browser!");
     }
 //Create database
-    db = window.sqlitePlugin.openDatabase({name: "my.db", "bgType":1});
-	console.log(db);}
-
+    db = window.sqlitePlugin.openDatabase({name: "my.db"});
+	console.log(db);
+	}
 function onSuccess(imageURI) {
     image = document.getElementById("photo");
     image.src = imageURI;
@@ -22,26 +22,29 @@ function onSuccess(imageURI) {
 	getLocation();
     //Prepare database
 	db.transaction(populateDB, errorCB, successCB);
-	
-
     function populateDB(tx) {
-        alert("Iam in populateDB");
+        console.log("populateDB started");
         //Create necessary tables
-        tx.exectuteSql('CREATE TABLE IF NOT EXISTS photo (photo_pk integer primary key, photopath text )');
-        tx.exectuteSql('CREATE TABLE IF NOT EXISTS  tag (tag_pk integer primary key, tagname text)');
-        tx.exectuteSql('CREATE TABLE IF NOT EXISTS mapper (map_id integer primary key, FOREIGN KEY tag_fk REFERENCES tag(tag_pk), FOREIGN KEY photo_fk REFERENCES photo(photo_pk)');
+		tx.executeSql('DROP TABLE IF EXISTS photo');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS photo (photo_pk integer primary key, photopath text )');
+		tx.executeSql('DROP TABLE IF EXISTS tag');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS  tag (tag_pk integer primary key, tagname text)');
+		tx.executeSql('DROP TABLE IF EXISTS mapper');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS mapper (map_id integer primary key, FOREIGN KEY tag_fk REFERENCES tag(tag_pk), FOREIGN KEY photo_fk REFERENCES photo(photo_pk)');
         console.log("tables created"); 
         //Insert the photo path to the photo table
-        tx.exectuteSql('INSERT INTO photo VALUES(?)', [(imageURI).val()], function (tx, res) {
-            console.log("insertId: " + res.insertId + " -- probably 1");
-            console.log("rowsAffected: " + res.rowsAffected + " -- should be 1");
+		tx.executeSql('INSERT INTO photo (photopath) VALUES(?)',  [ '+ imageURI +' ] , function (tx, res) {
+        console.log("insertId: " + res.insertId + " -- probably 1");
+        console.log("rowsAffected: " + res.rowsAffected + " -- should be 1");
         });
         //Check rows inserted in a photo table
-        tx.executeSql("SELECT count(photo_pk) as cnt from photo;", [], function (tx, res) {
-            console.log("res.rows.length: " + res.rows.length + " -- should be 1");
-            console.log("res.rows.item(0).cnt: " + res.rows.item(0).cnt + " -- should be 1");
+        tx.executeSql("SELECT count(photo_pk) as cnt FROM photo;", [], function (tx, res) {
+        console.log("res.rows.length: " + res.rows.length + " -- should be 1");
+        console.log("res.rows.item(0).cnt: " + res.rows.item(0).cnt + " -- should be 1");
+		console.log("populateDB finished");
         });
     }
+}
     // Transaction error callback    
     function errorCB(err) {
         alert("Error processing SQL: " + err.message);		
@@ -51,8 +54,6 @@ function onSuccess(imageURI) {
         console.log("success!");
     }
 
-
-}
 
 function onFail(message) {
     alert('Failed because: ' + message);
@@ -75,7 +76,8 @@ function getPhoto() {
     navigator.camera.getPicture(onSuccess, onFail, {
         quality: 100,
         destinationType: Camera.DestinationType.FILE_URI,
-        sourceType: Camera.PictureSourceType.CAMERA,
-        saveToPhotoAlbum: true
+        sourceType: Camera.PictureSourceType.CAMERA
+		// Hack do to android emulator error
+        //saveToPhotoAlbum: true
     });
 }
